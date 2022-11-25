db076204f2e175f99c1e8f3017376974ef7d4c2c
softontechsolution <softontechsolution@gmail.com>
2022-11-25 11:36:43 +0100

    folder structuring

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const PPrimaryColor = Color(0xFFDCF3FD);
+const PSecondarColor = Color(0xFFCEC3FD);
+const PAccentColor = Color(0xFFE290fa);
+
+const PButtonColor = Color(0xFF00002e);
+const PCardBgColor = Color(0xFFF7F6F1);
+const PWhiteColor = Color(0xFFFFFFFF);
---
+++ lib/constants/texts_string.dart
@@ -0,0 +1,23 @@
+// Generic text
+const String pLogin = "Login";
+const String pSignup = "Register";
+
+const String pSplashText = "Manage your finance with ease and comfort";
+const String pAppName = "PayLnk";
+const String pAppTagLine = "Payment on the go. \nBe in-control of your funds";
+
+//OnBoarding Screen
+const String pOnboardTitle1 = "Wallet like financial hub";
+const String pOnboardTitle2 = "Take control of your finances and expenses";
+const String pOnboardTitle3 = "Send and receive money fast & easy";
+const String pOnboardTitle4 = "Cross border payments";
+const String pOnboardTitle5 = "Manage your finance with ease and comfort";
+const String pOnboardSubtitle1 = "comes with robust features to make \ntransactions easy, fast and secured.";
+const String pOnboardSubtitle2 = "monitor your in-flow and out-flow of money \nand keep your spending in-check.";
+const String pOnboardSubtitle3 = "with advance security and efficient \ntechnology perform transactions at ease and speed.";
+const String pOnboardSubtitle4 = "sending money to friends and family \nin an different country is made ease and fast.";
+const String pOnboardSubtitle5 = "Payment on the go. \nBe in-control of your funds.";
+
+// Welcome Screen Text
+const String pWelcomeTitle = "Take control of your \nFinances today, Link your funds for \nEase and Convenience.";
+const String pWelcomeSubtitle = "Easy. Fast. Secure.";
